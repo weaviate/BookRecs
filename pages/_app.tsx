a6d47d9a76eeadce17c0f9c4c31cd02a72fff397
AJ Chan <adam@ajchan.io>
2024-03-15 16:39:13 +0100

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P9QFZP1Q1V"></script>
+        <Script strategy="lazyOnload" id="gtag-config">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-P9QFZP1Q1V');
+          `}
+        </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
+  
 }
 
 export default MyApp
