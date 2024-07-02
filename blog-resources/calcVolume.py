2bb996613362df2174aa5fd57f0f09fe80b69b41
AJ Chan <adam@ajchan.io>
2024-07-02 09:56:16 -0700

    Add blog resources for Apple Ecosystem Apps and Weaviate Blog

---
+++ blog-resources/calcVolume.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Sample calculation: Calculate the volume of a sphere with radius 5
+radius = 5
+volume = (4/3) * np.pi * (radius**3)
+
+# Print the volume with a label
+print("Volume of the sphere:", volume)
